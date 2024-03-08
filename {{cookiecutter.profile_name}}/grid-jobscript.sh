be27acd8a471a94018f397f33da0c9e9aca0264e
Alexander Puck Neuwirth <APN-Pucky@users.noreply.github.com>
2024-03-08 21:45:25 +0100

    Update grid-jobscript.sh

--- {{cookiecutter.profile_name}}/grid-jobscript.sh
+++ {{cookiecutter.profile_name}}/grid-jobscript.sh
@@ -3,7 +3,12 @@
 
 set -e
 
+F=/tmp/krb5cc_${{UID}}_$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 5)
+cp .ticket $F
+
 echo "hostname:"
 hostname -f
+#source /home_NFS/users/REPALCETHIS/gentoo/startprefix
+source /home_NFS/users/REPALCETHIS/snakemake/snek/bin/activate
 
 {exec_job}
